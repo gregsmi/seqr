8d6046c1ebf924eb81df802cebd1728ff8303e71
Greg Smith <gregsmi@microsoft.com>
2023-11-15 09:10:01 -0800

    parameterized ref storage account

--- reference_data/management/commands/update_pub_evidence.py
+++ reference_data/management/commands/update_pub_evidence.py
@@ -7,24 +7,28 @@ from reference_data.models import PubEvidence
 from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient
 
+from settings import AZURE_REF_STORAGE_ACCOUNT
+
 logger = logging.getLogger(__name__)
+
+
+ACCOUNT_URL = f"https://{AZURE_REF_STORAGE_ACCOUNT}.blob.core.windows.net"
+CONTAINER_NAME = "reference"
+BLOB_NAME = "evagg/truth_set_test.csv"
 
 
 class PubEvReferenceDataHandler(ReferenceDataHandler):
 
     model_cls = PubEvidence
-    url = "https://msseqr01sa.blob.core.windows.net/reference/evagg/truth_set_small_10-10-23.tsv"
-    account_url = "https://msseqr01sa.blob.core.windows.net"
-    container_name = "reference"
-    blob_name = "evagg/truth_set_small_10-10-23.tsv"
+    url = f"{ACCOUNT_URL}/{CONTAINER_NAME}/{BLOB_NAME}"
 
     def download_from_url(self):
         local_file_path = os.path.join(tempfile.gettempdir(), os.path.basename(self.url))
 
         # Download from Azure blob storage to local file using DefaultAzureCredential
-        blob_service_client = BlobServiceClient(account_url=self.account_url, credential=DefaultAzureCredential())
-        container_client = blob_service_client.get_container_client(self.container_name)
-        blob_client = container_client.get_blob_client(self.blob_name)
+        blob_service_client = BlobServiceClient(account_url=ACCOUNT_URL, credential=DefaultAzureCredential())
+        container_client = blob_service_client.get_container_client(CONTAINER_NAME)
+        blob_client = container_client.get_blob_client(BLOB_NAME)
 
         with open(local_file_path, "wb") as f:
             download_stream = blob_client.download_blob()
