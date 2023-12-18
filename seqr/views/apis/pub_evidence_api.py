5140a2d51356a37b1929b8d35e9bae0d5221d14c
SEQR UI Dev <gregsmi@microsoft.com>
2023-12-18 22:31:46 +0000

    add all evidence api

--- seqr/views/apis/pub_evidence_api.py
+++ seqr/views/apis/pub_evidence_api.py
@@ -1,3 +1,5 @@
+from django.db.models import Count
+
 from reference_data.models import PubEvidence
 from seqr.views.utils.json_utils import create_json_response
 #from seqr.views.utils.note_utils import create_note_handler, update_note_handler, delete_note_handler
@@ -5,10 +7,21 @@ from seqr.views.utils.orm_to_json_utils import get_json_for_pub_evidence
 from seqr.views.utils.permissions_utils import login_and_policies_required
 
 
+def get_pub_evidence(gene_id):
+    return PubEvidence.objects.filter(gene__gene_id=gene_id)
+
+
 @login_and_policies_required
 def pub_evidence(request, gene_id):
-    evidences = PubEvidence.objects.filter(gene__gene_id=gene_id)
+    evidences = get_pub_evidence(gene_id)
     pub_evidence = {gene_id: get_json_for_pub_evidence(evidences, user=request.user)}
+    return create_json_response({'pubEvByGene': pub_evidence})
+
+
+@login_and_policies_required
+def pub_evidences(request):
+    gene_ids = PubEvidence.objects.values_list('gene__gene_id', flat=True).distinct()
+    pub_evidence = { gene_id: get_json_for_pub_evidence(get_pub_evidence(gene_id), user=request.user) for gene_id in gene_ids }
     return create_json_response({'pubEvByGene': pub_evidence})
 
 
--- seqr/views/apis/pub_evidence_api_tests.py
+++ seqr/views/apis/pub_evidence_api_tests.py
@@ -1,6 +1,6 @@
 from django.urls.base import reverse
 
-from seqr.views.apis.pub_evidence_api import pub_evidence
+from seqr.views.apis.pub_evidence_api import pub_evidence, pub_evidences
 from seqr.views.utils.test_utils import AuthenticationTestCase
 
 
@@ -19,6 +19,18 @@ class PubEvidenceAPITest(AuthenticationTestCase):
 
         evs = response.json()['pubEvByGene']
         self.assertEqual(len(evs), 1)
+        self.assertTrue(GENE_ID in evs)
+        self.assertEqual(len(evs[GENE_ID]), 4)
+
+    def test_all_pub_evidence(self):
+        url = reverse(pub_evidences)
+        self.check_require_login(url)
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        evs = response.json()['pubEvByGene']
+        self.assertEqual(len(evs), 4)
         self.assertTrue(GENE_ID in evs)
         self.assertEqual(len(evs[GENE_ID]), 4)
 
