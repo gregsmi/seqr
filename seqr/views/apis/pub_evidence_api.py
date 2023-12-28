0425583dd803dd60331e39925e02003c77ffe417
SEQR UI Dev <gregsmi@microsoft.com>
2023-12-28 22:08:09 +0000

    updated urls and API

--- seqr/views/apis/pub_evidence_api.py
+++ seqr/views/apis/pub_evidence_api.py
@@ -8,51 +8,20 @@ from seqr.views.utils.orm_to_json_utils import get_json_for_pub_evidence, get_js
 from seqr.views.utils.permissions_utils import login_and_policies_required
 
 
-def _get_pub_evidence(gene_id):
-    return PubEvidence.objects.filter(gene__gene_id=gene_id)
-
-
-def _get_pub_evidence_note_by_guid(note, user):
-    return {'pubEvidenceNotesByGuid': {note.guid: get_json_for_pub_ev_note(note, user)}}
-
-
-@login_and_policies_required
-def pub_evidence(request):
-    gene_ids = PubEvidence.objects.values_list('gene__gene_id', flat=True).distinct()
-    pub_evidence = { gene_id: get_json_for_pub_evidence(_get_pub_evidence(gene_id), request.user) for gene_id in gene_ids }
-    return create_json_response({'pubEvidenceByGene': pub_evidence})
-
-
 @login_and_policies_required
 def pub_evidence_for_gene(request, gene_id):
-    evidences = _get_pub_evidence(gene_id)
-    pub_evidence = {gene_id: get_json_for_pub_evidence(evidences, request.user)}
-    return create_json_response({'pubEvidenceByGene': pub_evidence})
-
-
-@login_and_policies_required
-def pub_evidence_notes_for_gene(request, gene_id):
-    notes = PubEvidenceNote.objects.filter(gene_id=gene_id)
-    notes_by_gene = {gene_id: get_json_for_pub_ev_note(note, request.user) for note in notes}
-    return create_json_response({'pubEvidenceNotesByGene': notes_by_gene})
-
-
-@login_and_policies_required
-def create_pub_evidence_gene_note(request, gene_id):
-    return create_note_handler(
-        request, PubEvidenceNote,
-        get_response_json=lambda note: _get_pub_evidence_note_by_guid(note, request.user),
-        required_fields=['noteType', 'note'], optional_fields=['pubEvId'],
-        gene_id=gene_id,
-    )
+    evidence = {gene_id: get_json_for_pub_evidence(PubEvidence.objects.filter(gene__gene_id=gene_id), request.user)}
+    gene_notes = PubEvidenceNote.objects.filter(gene_id=gene_id)
+    notes = { note.guid: get_json_for_pub_ev_note(note, request.user) for note in gene_notes }
+    return create_json_response({'pubEvidenceByGene': evidence, 'pubEvidenceNotesByGuid': notes})
 
 
 @login_and_policies_required
 def create_pub_evidence_note(request):
     return create_note_handler(
         request, PubEvidenceNote,
-        get_response_json=lambda note: _get_pub_evidence_note_by_guid(note, request.user),
-        required_fields=['noteType', 'pubEvId'], optional_fields=['note'],
+        get_response_json=lambda note: _get_pub_evidence_note_by_guid(note.guid, note, request.user),
+        required_fields=['geneId', 'noteType'], optional_fields=['pubEvId', 'note', 'noteStatus'],
     )
 
 
@@ -60,7 +29,7 @@ def create_pub_evidence_note(request):
 def update_pub_evidence_note(request, note_guid):
     return update_note_handler(
         request, PubEvidenceNote, note_guid,
-        get_response_json=lambda note: _get_pub_evidence_note_by_guid(note, request.user),
+        get_response_json=lambda note: _get_pub_evidence_note_by_guid(note.guid, note, request.user),
     )
 
 
@@ -68,5 +37,10 @@ def update_pub_evidence_note(request, note_guid):
 def delete_pub_evidence_note(request, note_guid):
     return delete_note_handler(
         request, PubEvidenceNote, note_guid,
-        get_response_json=lambda: {'pubEvidenceNotesByGuid': {note_guid: None}},
+        get_response_json=lambda: _get_pub_evidence_note_by_guid(note_guid, None, request.user)
     )
+
+
+def _get_pub_evidence_note_by_guid(note_guid, note, user):
+    note_body = None if note is None else get_json_for_pub_ev_note(note, user)
+    return {'pubEvidenceNotesByGuid': {note_guid: note_body}}
--- seqr/views/apis/pub_evidence_api_tests.py
+++ seqr/views/apis/pub_evidence_api_tests.py
@@ -2,7 +2,7 @@ import json
 from django.urls.base import reverse
 import mock
 
-from seqr.views.apis.pub_evidence_api import pub_evidence, pub_evidence_for_gene, create_pub_evidence_gene_note, \
+from seqr.views.apis.pub_evidence_api import pub_evidence_for_gene, \
     create_pub_evidence_note, update_pub_evidence_note, delete_pub_evidence_note
 from seqr.views.utils.test_utils import AuthenticationTestCase
 
@@ -14,18 +14,6 @@ PUB_EV_NOTE_FIELDS = {'noteGuid', 'note', 'geneId', 'lastModifiedDate', 'created
 
 class PubEvidenceAPITest(AuthenticationTestCase):
     fixtures = ['users', 'reference_data']
-
-    def test_pub_evidence(self):
-        url = reverse(pub_evidence)
-        self.check_require_login(url)
-
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-
-        evs = response.json()['pubEvidenceByGene']
-        self.assertEqual(len(evs), 4)
-        self.assertTrue(GENE_ID in evs)
-        self.assertEqual(len(evs[GENE_ID]), 4)
 
     def test_pub_evidence_for_gene(self):
         url = reverse(pub_evidence_for_gene, args=[GENE_ID])
@@ -40,17 +28,17 @@ class PubEvidenceAPITest(AuthenticationTestCase):
         self.assertEqual(len(evs[GENE_ID]), 4)
 
     def test_create_update_and_delete_pub_ev_note(self):
-        for create_note_url, note in [
-            (reverse(create_pub_evidence_gene_note, args=[GENE_ID]), {'noteType': 'F', 'note': 'new gene note'}),
-            (reverse(create_pub_evidence_note), {'noteType': 'N', 'pubEvId': PUB_EV_ID})
+        for type, note in [
+            ('gene note', {'noteType': 'F', 'geneId': GENE_ID, 'note': 'new gene note'}),
+            ('pub_ev note', {'noteType': 'N', 'geneId': GENE_ID, 'pubEvId': PUB_EV_ID, 'noteStatus': 'V'})
         ]:
-            with self.subTest(create_note_url):
-                # create the note
+            with self.subTest(type):
+                create_note_url = reverse(create_pub_evidence_note)
                 self.check_require_login(create_note_url)
-                response = self.client.post(create_note_url, content_type='application/json', data=json.dumps({}))
 
+                response = self.client.post(create_note_url, content_type='application/json', data=json.dumps({}))
                 self.assertEqual(response.status_code, 400)
-                self.assertDictEqual(response.json(), {'error': 'Missing required field(s): ' + ', '.join(note.keys())})
+                self.assertDictEqual(response.json(), {'error': 'Missing required field(s): geneId, noteType'})
 
                 response = self.client.post(create_note_url, content_type='application/json', data=json.dumps(note))
                 self.assertEqual(response.status_code, 200)
@@ -63,9 +51,10 @@ class PubEvidenceAPITest(AuthenticationTestCase):
                 self.assertSetEqual(set(new_note_response.keys()), PUB_EV_NOTE_FIELDS)
                 self.assertEqual(new_note_response['noteGuid'], new_note_guid)
                 self.assertEqual(new_note_response['noteType'], note['noteType'])
+                self.assertEqual(new_note_response['geneId'], note['geneId'])
                 self.assertEqual(new_note_response['pubEvId'], note.get('pubEvId', ''))
+                self.assertEqual(new_note_response['noteStatus'], note.get('noteStatus', 'N'))
                 self.assertEqual(new_note_response['note'], note.get('note', ''))
-                self.assertEqual(new_note_response['noteStatus'], 'N')
                 self.assertEqual(new_note_response['createdBy'], 'Test No Access User')
 
                 # update the note
